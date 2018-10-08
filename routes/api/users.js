f475bbe21a0f64105924bc19c65bceec4c24a8c5
NenadDjuricic <shone1995@gmail.com>
2018-10-08 06:14:29 +0200

    moguce preko postmana da se napravi user sa sakrivenim paswordom

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 //load user model
 const User = require('../../models/User');
 
@@ -24,12 +25,27 @@ router.post('/register', (req, res) => {
                     email: 'Email already exists!'
                 });
             } else {
-                const newUser = newUser({
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', //size
+                    r: 'pg', //rating
+                    d: 'mm' //default
+                });
+                const newUser = new User({
                     name: req.body.name,
                     email: req.body.email,
-                    avatar: avatar,
+                    avatar,
                     password: req.body.password
                 });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                })
             }
         });
 
