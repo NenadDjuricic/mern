f17cd413084b2799aac1a1ed1e5c10a281562cc0
NenadDjuricic <shone1995@gmail.com>
2018-10-08 05:06:21 +0200

    dodat user.js

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create scheme
+const UserScheme = new Schema({
+    name: {
+        type: string,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+module.exports = Users = mongoose.model('users', UserScheme);
